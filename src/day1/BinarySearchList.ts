04319ebcb31d94ac8ecefda49ada73b9949ac12c
Henrique <henriquesg09@hotmail.com>
2022-08-16 12:57:09 -0300

    implement binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    do {
+        const middle = Math.floor(lo + (hi - lo) / 2);
+        const value = haystack[middle];
+
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            hi = middle;
+        } else {
+            lo = middle + 1;
+        }
+    } while (lo < hi);
+
+    return false;
 }
