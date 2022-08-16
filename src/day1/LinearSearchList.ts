7f3b0b64cb2a9fd94272dfcb32ceb91884c151f4
Henrique <henriquesg09@hotmail.com>
2022-08-16 13:26:51 -0300

    implement bubble sort

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,9 +1,13 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
-  for (let i = 0; i < haystack.length; ++i) {
-    if (haystack[i] === needle) {
-      return true;
+// O(n)
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (let i = 0; i < haystack.length; ++i) {
+        if (haystack[i] === needle) {
+            return true;
+        }
     }
-  }
 
-  return false;
+    return false;
 }
