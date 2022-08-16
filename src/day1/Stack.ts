d6edd1541fb7a51bf68834a58d8d6b443f0a1dd1
Henrique <henriquesg09@hotmail.com>
2022-08-16 15:15:43 -0300

    implement stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,45 @@
+type SNode<T> = {
+    value: T;
+    prev?: SNode<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: SNode<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = { value: item } as SNode<T>;
 
-}
+        this.length++;
+
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+    }
+
     pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
 
-}
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined;
+            return head?.value;
+        }
+
+        const head = this.head as SNode<T>;
+        this.head = head.prev;
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
